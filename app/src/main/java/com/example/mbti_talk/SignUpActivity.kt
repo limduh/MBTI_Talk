8afd0a328997431fe3b886e3efe58cd22d8584b5
황진주 <ghkd1007@gmail.com>
2023-10-16 10:47:49 +0900

    [ui] 다이얼로그 추가, 회원가입페이지 수정

--- app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
+++ app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        setContentView(R.layout.activity_sign_up)
     }
 }
