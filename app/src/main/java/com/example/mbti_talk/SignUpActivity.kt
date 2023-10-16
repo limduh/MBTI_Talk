63e0b379cff0b39241c2925ec9976ef3bc711eea
Lee do yeon <simon3397@gmail.com>
2023-10-16 21:40:05 +0900

    [FEAT] 파베 DB 초기화, 버튼 클릭 리스너 설정(realtime DB 에 데이터 넘겨짐)

--- app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
+++ app/src/main/java/com/example/mbti_talk/SignUpActivity.kt
@@ -7,47 +7,56 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mbti_talk.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
+    // 뷰바인딩
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    // 파이어베이스 데이터베이스 초기화
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-////////
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
-
+        database = Firebase.database.reference
 
         binding.SignUpBtnSignUp.setOnClickListener {
             val SignupActivity_id = binding.SignUpEtxtID.text.toString()
-            val SignupActivity_pass = binding.SignUpEtxtPw.text.toString()
-            val SignupActivity_confirmPass = binding.SignUpEtxtPwCheck.text.toString()
+            val SignupActivity_nickname = binding.SignUpEtxtPw.text.toString()
+            val SignupActivity_age = binding.SignUpEtxtAge.text.toString().toInt()
+            val SignupActivity_sex = binding.SignUpEtxtNickName.text.toString()
 
+            val user = UserData(SignupActivity_id, SignupActivity_nickname, SignupActivity_age, SignupActivity_sex)
 
-            if (SignupActivity_id.isNotEmpty() && SignupActivity_pass.isNotEmpty() && SignupActivity_confirmPass.isNotEmpty()) {
-                if (SignupActivity_pass == SignupActivity_confirmPass) {
+            database.child("users").child(SignupActivity_id).setValue(user)
 
-                    Toast.makeText(this, "회원가입이 완료되었습니다.로그인 해주세요", Toast.LENGTH_SHORT).show()
-                    firebaseAuth.createUserWithEmailAndPassword(SignupActivity_id, SignupActivity_pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, LogInActivity::class.java)
-                            startActivity(intent)
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
-                        }
-                    }
-                } else {
-                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "작성하지 않은곳이 있네요 !!", Toast.LENGTH_SHORT).show()
-
-            }
+//            if (SignupActivity_id.isNotEmpty() && SignupActivity_pass.isNotEmpty() && SignupActivity_confirmPass.isNotEmpty()) {
+//                if (SignupActivity_pass == SignupActivity_confirmPass) {
+//
+//                    Toast.makeText(this, "회원가입이 완료되었습니다.로그인 해주세요", Toast.LENGTH_SHORT).show()
+//                    firebaseAuth.createUserWithEmailAndPassword(SignupActivity_id, SignupActivity_pass).addOnCompleteListener {
+//                        if (it.isSuccessful) {
+//                            val intent = Intent(this, LogInActivity::class.java)
+//                            startActivity(intent)
+//                        } else {
+//                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+//
+//                        }
+//                    }
+//                } else {
+//                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+//                }
+//            } else {
+//                Toast.makeText(this, "작성하지 않은곳이 있네요 !!", Toast.LENGTH_SHORT).show()
+//
+//            }
         }
     }
 }
