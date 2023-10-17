3a0d71ba77aed48100018be86b0d26684a471a38
jh4016 <wlsgur_e@naver.com>
2023-10-17 14:29:46 +0900

    [login.test]

---
+++ app/src/main/java/com/example/mbti_talk/Porfile.kt
@@ -0,0 +1,27 @@
+package com.example.mbti_talk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mbti_talk.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+
+
+
+class Porfile : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val userId = firebaseAuth.currentUser?.uid
+
+
+        }
+    }
