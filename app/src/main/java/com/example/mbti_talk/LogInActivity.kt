2454f2453ace3ae02570f8848471644604791d4e
jh4016 <wlsgur_e@naver.com>
2023-10-17 21:33:34 +0900

    [FEAT]⚙️ 회원가입정보 인텐트로 넘기기

--- app/src/main/java/com/example/mbti_talk/LogInActivity.kt
+++ app/src/main/java/com/example/mbti_talk/LogInActivity.kt
@@ -3,6 +3,7 @@ package com.example.mbti_talk
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract.Profile
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
@@ -18,45 +19,52 @@ class LogInActivity : AppCompatActivity() {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val etxtId = binding.LoginEtxtId
-//        val etxtPassword = binding.LoginEtxtPassword
+        val etxtId = binding.LoginEtxtId
+        val etxtPassword = binding.LoginEtxtPassword
         val btnLogin = binding.LoginBtnLogin
         val btnSignUp = binding.loginTxtSignUp
         firebaseAuth = FirebaseAuth.getInstance()
-        val etxtId="test1@naver.com"
-        val password="sdjkdj"
+        val get_email=intent.getStringExtra("email")
+        val get_password=intent.getStringExtra("password")
+
+        //회원가입시 바로 로그인 정보 입력
+        etxtId.setText(get_email)
+        etxtPassword.setText(get_password)
+
 
         btnLogin.setOnClickListener {
-//            val id = etxtId.text.toString()
-//            val password = etxtPassword.text.toString()
-//
-//            if (id.isEmpty() || password.isEmpty()) {
-//                // 아이디 또는 비밀번호가 입력X
-//                Toast.makeText(this, "아이디와 비밀번호를 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
-//            } else {
-                resultLogin(etxtId, password)
+            val id = etxtId.text.toString()
+            val password = etxtPassword.text.toString()
+
+            if (id.isEmpty() || password.isEmpty()) {
+                // 아이디 또는 비밀번호가 입력X
+                Toast.makeText(this, "아이디와 비밀번호를 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                resultLogin(id, password)
             }
+        }
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-        }
-//    }
-
-    private fun resultLogin(id: String, pass: String) {
-        firebaseAuth.signInWithEmailAndPassword(id, pass)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // 로그인 성공 토스팅
-                    Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-                    // 메인 페이지 이동
-                    val intent = Intent(this, Porfile::class.java)
-                    startActivity(intent)
-                    finish() // 로그인 화면 종료
-                } else {
-                    //로그인 실패
-                    Toast.makeText(this, "아이디 또는 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
-                }
-            }
     }
-}
+
+
+
+        private fun resultLogin(id: String, pass: String) {
+            firebaseAuth.signInWithEmailAndPassword(id, pass)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인 성공 토스팅
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        // 메인 페이지 이동
+                        val intent = Intent(this, Porfile::class.java)
+                        startActivity(intent)
+                        finish() // 로그인 화면 종료
+                    } else {
+                        //로그인 실패
+                        Toast.makeText(this, "아이디 또는 비밀번호가 올바르지 않습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
