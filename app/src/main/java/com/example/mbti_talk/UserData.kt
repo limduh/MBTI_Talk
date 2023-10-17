16163ec937c9e7974f2bba0eec4e49b6b6e48d20
Lee do yeon <simon3397@gmail.com>
2023-10-17 11:26:24 +0900

    [LITTLE_FIX]

--- app/src/main/java/com/example/mbti_talk/UserData.kt
+++ app/src/main/java/com/example/mbti_talk/UserData.kt
@@ -1,10 +1,9 @@
 package com.example.mbti_talk
 
 data class UserData(
-    val user_id: String = "",
-    val user_pass: String = "",
-    val user_confirmPass: String = "",
+    val user_email: String = "",
     val user_age: Int = 0,
     val user_nickName: String = "",
-    val uid: String = ""
+    val uid: String = "",
+    val gender: String = ""
 )
