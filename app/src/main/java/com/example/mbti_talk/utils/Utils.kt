cde84b0488eaee6b379348a3a9ff2316b381922a
황진주 <ghkd1007@gmail.com>
2023-10-19 14:35:37 +0900

    [feat] utils 작성중,

---
+++ app/src/main/java/com/example/mbti_talk/utils/Utils.kt
@@ -0,0 +1,32 @@
+package com.example.mbti_talk.utils
+
+import android.app.Activity
+import android.content.Context
+import com.example.mbti_talk.UserData
+
+class Utils {
+
+
+//Utils자료 참고 // 강의내용 참고 코드
+    //SharedPreferences와 Preferences의 차이 : 여러개의 정보를 저장할거냐 or 정보한개만 저장할거냐 인데,
+    //보통 한개만 하지 않으므로 여러개를 저장해주는 SharedPreferences를 사용한다.
+
+    //getSharedPreferences의 형식 :
+    //getSharedPreferences(name, mode)
+
+    //name : 프리퍼런스 데이터를 저장할 xml파일의 이름이다. (자유롭게 설정가능)
+    //mode : 파일의 공유 모드이다.
+
+    //여기서 내가 사용한 MODE_PRIVATE은, 생성된 xml파일은, 호출한 애플리케이션내에서
+    //(즉,우리 mbtiTalk의 앱 내에서만 ) 읽기 쓰기가 가능하다.
+
+
+
+    private fun saveData(context: Context, item: UserData ) {
+        val prefs = context.getSharedPreferences("UserInfo", Activity.MODE_PRIVATE)
+        val editor = prefs.edit()
+        val gson = G
+
+    }
+
+}
