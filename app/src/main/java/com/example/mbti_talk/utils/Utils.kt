2f61682d5bbe4c4248ec600fe7254987b278ed2d
황진주 <ghkd1007@gmail.com>
2023-10-19 15:10:17 +0900

    [feat] util작성완료1차

--- app/src/main/java/com/example/mbti_talk/utils/Utils.kt
+++ app/src/main/java/com/example/mbti_talk/utils/Utils.kt
@@ -1,8 +1,10 @@
 package com.example.mbti_talk.utils
 
 import android.app.Activity
+import android.content.ClipData.Item
 import android.content.Context
 import com.example.mbti_talk.UserData
+import com.google.gson.GsonBuilder
 
 class Utils {
 
@@ -20,13 +22,47 @@ class Utils {
     //여기서 내가 사용한 MODE_PRIVATE은, 생성된 xml파일은, 호출한 애플리케이션내에서
     //(즉,우리 mbtiTalk의 앱 내에서만 ) 읽기 쓰기가 가능하다.
 
+    //GsonBuilder를 사용하기 위해선 그래들에, gson관련
+    //    // gson converter
+    //    implementation("com.google.code.gson:gson:2.10.1")
+    //    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //해당 그래들을 추가해야 사용할 수 있다.
 
-
-    private fun saveData(context: Context, item: UserData ) {
+    private fun saveData(context: Context, item: UserData) {
         val prefs = context.getSharedPreferences("UserInfo", Activity.MODE_PRIVATE)
         val editor = prefs.edit()
-        val gson = G
+        val gson = GsonBuilder().create()
+        editor.putString("save", gson.toJson(item))
+        //첫번째는 키, 2번째는 실제 담아둘 값.
+        editor.apply()
+    }
 
+    private fun loadData(context: Context): ArrayList<Item> {
+        val prefs = context.getSharedPreferences("UserInfo", Activity.MODE_PRIVATE)
+        //여기서 Activity는 왜쓰이는지 모름. 물어보기
+        val allEntries: Map<String, *> = prefs.all
+        val load = ArrayList<Item>()
+        val gson = GsonBuilder().create()
+        for ((key, value) in allEntries) {
+            val item = gson.fromJson(value as String, Item::class.java)
+            load.add(item)
+        }
+        return load
     }
 
 }
+
+// 참고 코드
+//    fun getPrefBookmarkItems(context: Context): ArrayList<Item> {
+//        val prefs = context.getSharedPreferences("videoID", Activity.MODE_PRIVATE)
+//        val allEntries: Map<String, *> = prefs.all
+//        val bookmarkItems = ArrayList<Item>()
+//        val gson = GsonBuilder().create()
+//        for ((key, value) in allEntries) {
+//            val item = gson.fromJson(value as String, Item::class.java)
+//            bookmarkItems.add(item)
+//        }
+//        return bookmarkItems
+//    }
+
+
