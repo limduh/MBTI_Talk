7a14b9bb8b7adb5873de78045e36fbf71d12ed26
Lee do yeon <simon3397@gmail.com>
2023-10-24 11:49:24 +0900

    [FEAT] main 에서 특정 user 클릭 시 DetailActivity 에 user nickname, age, gender, mbti 보여짐

--- app/src/main/java/com/example/mbti_talk/DetailActivity.kt
+++ app/src/main/java/com/example/mbti_talk/DetailActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatTextView
 import com.example.mbti_talk.Adapter.UserAdapter
 import com.example.mbti_talk.databinding.ActivityDetailBinding
 import com.example.mbti_talk.databinding.ActivityMainBinding
@@ -13,15 +14,19 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.core.Constants
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class DetailActivity : AppCompatActivity() {
 
     // DetailActivity 클래스의 멤버 변수들을 선언
     lateinit var binding: ActivityDetailBinding // 뷰바인딩 초기화
-
     lateinit var detailDB: DatabaseReference // FB Realtime DB와 연동하기 위한 레퍼런스를 초기화
 
+    private lateinit var nameTextView: AppCompatTextView
+    private lateinit var ageTextView: AppCompatTextView
+    private lateinit var genderTextView: AppCompatTextView
+    private lateinit var mbtiTextView: AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +35,12 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        nameTextView = binding.DetailTxtNickname
+        ageTextView = binding.DetailTxtAge
+        genderTextView = binding.DetailTxtGender
+        mbtiTextView = binding.DetailTxtMbti
+
+        // MainActivity 에서 넘어온 intent 를 받아서 userID 변수에 저장
         val userID = intent.getStringExtra("userId")
 
         Log.d("DetailActivity", "userID = $userID")
@@ -38,8 +49,25 @@ class DetailActivity : AppCompatActivity() {
         // RDB 에서 사용자 데이터 가져오기
         detailDB.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                // 가져온 데이터를 UserData 객체로 변환
-                for (snapshot in snapshot.children) {
+                Log.d("DetailActivity", "snapshot.exists() = ${snapshot.exists()}")
+                if (snapshot.exists()) {
+                    lateinit var userData : DataSnapshot
+                    if (userID != null) {
+                        userData = snapshot.child(userID)
+                    } else {
+                        Toast.makeText(this@DetailActivity, "사용자 정보를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    // 데이터에서 이름, 나이, 성별, MBTI 정보 가져오기
+                    val name = userData.child("user_nickName").getValue<String?>()
+                    val age = userData.child("user_age").getValue<Int?>()
+                    val gender = userData.child("user_gender").getValue<String?>()
+                    val mbti = userData.child("user_mbti").getValue<String?>()
+
+                    // 가져온 데이터를 TextView에 설정
+                    nameTextView.text = name
+                    ageTextView.text = age.toString()
+                    genderTextView.text = gender
+                    mbtiTextView.text = mbti
                 }
             }
             override fun onCancelled(error: DatabaseError) {
@@ -47,18 +75,5 @@ class DetailActivity : AppCompatActivity() {
                 Toast.makeText(this@DetailActivity, "데이터를 가져오는데 실패했습니다.", Toast.LENGTH_SHORT).show()
             }
         })
-
-
-
-//        // userData 를 사용하여 Detail 정보 표시
-//        // ex) nickName 표시하는 TextView 를 찾아서 설정
-//        val detailNickname = binding.DetailTxtNickname
-//        detailNickname.text = userDetail.user_nickName
-//
-//        val detailMbti = binding.DetailTxtMbti
-//        detailMbti.text = userDetail.user_mbti
-//
-//        val detailAge = binding.DetailTxtAge
-//        detailAge.text = userDetail.user_age.toString()
     }
 }
