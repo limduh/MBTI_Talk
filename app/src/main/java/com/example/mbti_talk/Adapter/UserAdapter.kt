3d5183f36393084f30e83581a33d4e82b2c4120e
Lee do yeon <simon3397@gmail.com>
2023-10-24 11:49:45 +0900

    [DOCS] 주석 추가

--- app/src/main/java/com/example/mbti_talk/Adapter/UserAdapter.kt
+++ app/src/main/java/com/example/mbti_talk/Adapter/UserAdapter.kt
@@ -33,6 +33,14 @@ class UserAdapter(private  val mContext: Context, private val userList: List<Use
     // onBindViewHolder 는 RecyclerView 가 화면에 표시할 때 호출.
     // position 은 현재 ViewHolder 가 Binding 될 항목의 위치를 나타냄
     // userList[position] 을 사용하여 해당 위치의 UserData 가져옴
+
+    /* onBindViewHolder는 RecyclerV가 화면에 Item 표시 or Item 이 화면에 스크롤되거나 새 아이템 나타날 때 호출
+    holder 는 RV,ViewHolder 객체. Item View 의 레이아웃 내부의 서브 뷰 접근할 수 있다.
+    position 은 현재 Item 위치 나타내는 Index. 이 위치에 해당하는 Data 를 가져와 Item View 에 표시
+
+    onBindViewHolder 내에서는 주로 position 매개변수 사용하여 Data 를 가져와서 ViewHolder 내부 View 에 설정
+    * */
+
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val user = userList[position]
 
@@ -51,26 +59,18 @@ class UserAdapter(private  val mContext: Context, private val userList: List<Use
             intent.putExtra("userId", user.user_uid)
             startActivity(mContext, intent, null)
         }
-        // 클릭 이벤트를 처리한 후, Holder(binding)을 반환
-
     }
-
-    /* onBindViewHolder는 RecyclerV가 화면에 Item 표시 or Item 이 화면에 스크롤되거나 새 아이템 나타날 때 호출
-    holder 는 RV,ViewHolder 객체. Item View 의 레이아웃 내부의 서브 뷰 접근할 수 있다.
-    position 은 현재 Item 위치 나타내는 Index. 이 위치에 해당하는 Data 를 가져와 Item View 에 표시
-
-    onBindViewHolder 내에서는 주로 position 매개변수 사용하여 Data 를 가져와서 ViewHolder 내부 View 에 설정
-    * */
 
     // Holder 클래스는 ViewHolder 패턴을 구현
     // XML 레이아웃에서 정의한 뷰들을 멤버 변수로 가진다.
     // 이 뷰들에 유저 데이터 설정, 아이템 이벤트를 처리
+
     inner class Holder(binding: UserListBinding) : RecyclerView.ViewHolder(binding.root) {
         val user_list = binding.clListItem // 유저 리스트 바인딩
         val user_nickname = binding.etNickName // 닉네임 텍스트뷰
         val user_age = binding.etAge // 나이 텍스트뷰
         val user_gender = binding.etGender // 성별 텍스트뷰
         val user_mbti = binding.etMbti // mbti 텍스트뷰
-        val user_profile = binding.ivProfile // 프로필 이미지뷰
+//        val user_profile = binding.ivProfile // 프로필 이미지뷰
     }
 }
