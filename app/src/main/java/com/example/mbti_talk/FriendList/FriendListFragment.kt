90d3b8ee631d394d2e5f4ccb682b6a08cdee1322
Lee do yeon <simon3397@gmail.com>
2023-10-26 05:17:42 +0900

    [DOCS] 주석 추가

--- app/src/main/java/com/example/mbti_talk/FriendList/FriendListFragment.kt
+++ app/src/main/java/com/example/mbti_talk/FriendList/FriendListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mbti_talk.Adapter.UserAdapter
@@ -18,6 +19,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
+// RDB 사용하여 친구목록을 가져와 RecyclerView에 표시
 class FriendListFragment : Fragment() {
 
     private lateinit var binding: FragmentFriendListBinding
@@ -52,74 +54,90 @@ class FriendListFragment : Fragment() {
         // 친구 데이터 목록 및 RDB 초기화
         userDB = Firebase.database.reference.child("Users")
         friendDB = Firebase.database.reference.child("Friends")
+
+        val currentUserUid = Utils.getMyUid(requireContext()) // util 함수 통해 현재 사용자 uid 가져오기
+        loadFriends(currentUserUid.toString()) // 현재 사용자 uid를 통해 친구 목록 가져오기
+
         friendadapter = UserAdapter(requireContext(), friendList) // Rv 에 사용될 어댑터 초기화
 
         // RecyclerView에 어댑터 설정
         binding.friendlistFragRv.adapter = friendadapter
         binding.friendlistFragRv.layoutManager = LinearLayoutManager(requireContext())
-
-
     }
 
-
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("FriendListFragment", "onResume")
-        val currentUserUid = Utils.getMyUid(requireContext())
-        loadFriends(currentUserUid.toString())
-    }
-
+    // 현재 사용자의 uid 기반으로 친구 목록을 로드하는 함수. 친구 수 계산 후 각 친구 uid 반복하여 'loadFriendData' 함수 사용하여 친구 데이터 불러옴. friendAdapter 는 모든 친구 데이터를 불러온 후 한번만 알림을 받음.
     private fun loadFriends(currentUserUid: String) {
+        // 중복 로드 피하기 위해 friendList 를 지움.
+        friendList.clear()
+
+        // 현재 유저의 친구 데이터베이스를 쿼리합니다. (정보요청)
         friendDB
-            .child(currentUserUid)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
+            .child(currentUserUid) // friendDb 아무래 currentUserUid 를 키로 갖는 하위 노드 찾음.
+            .addListenerForSingleValueEvent(object : ValueEventListener { // alfsv 함수는 데이터 변경을 단 한번만 기다림.
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // RDB 에서 데이터 검색 성공 시 실행되는 콜백 함수.
                 Log.d("FirebaseDatabase", "loadFriends onDataChange")
 
+                // 추가한 친구 uid 가 userDB 에 존재하는지 확인
                 if (dataSnapshot.exists()) {
+                    // 유저의 친구 수 계산. datasnapshot = 현재 사용자의 친구 목록 데이터
                     val size = dataSnapshot.children.count()
+                    // 로그에 친구 수 출력
                     Log.d("FirebaseDatabase", "dataSnapshot.exists() size = $size")
                     var cnt = 0
+                    // 각 하위 노드(친구 uid) 반복 처리
                     for (friendUidSnapshot in dataSnapshot.children) {
+                        // 하위 노드에서 친구 uid 추출
                         val friendUid = friendUidSnapshot.key
+                        // 친구 uid가 null이 아닌지 확인. null 이면 데이터 안부름.
                         if (friendUid != null) {
+                            // 각 친구 uid에 loadFriendData 함수를 통해 친구 데이터를 가져옴.
+                            // true, false 는 마지막 친구 데이터를 가져왔는지 확인하기 위한 변수.
+                            // friendadaper 가 모든 친구 데이터 불러온 후 한번만 알림을 받게 함.
                             if(cnt==size-1)
                                 loadFriendData(friendUid,true)
                             else
                                 loadFriendData(friendUid,false)
                         }
-                        cnt++
+                        cnt++ // 처리한 친구 수 추적 위해 cnt 변수 증가
                     }
                 } else {
+                    // 현재 사용자의 친구가 없다면, 친구를 찾을 수 없다는 로그 남김
                     Log.d("FirebaseDatabase", "No friends found for UID: $currentUserUid")
+                    Toast.makeText(requireContext(), "친구를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
 
-            override fun onCancelled(databaseError: DatabaseError) {
+            override fun onCancelled(databaseError: DatabaseError) { // DB 오류 처리하고 메시지 로깅
                 Log.d("FirebaseDatabase", "onCancelled", databaseError.toException())
             }
         })
     }
 
+    // 친구 uid를 통해 친구 데이터를 가져오는 함수
     private fun loadFriendData(friendUid: String, isLast:Boolean) {
+        // userDB 에서 frienUid 에 해당하는 데이터 조회
         userDB
             .child(friendUid)
             .addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(friendDataSnapshot: DataSnapshot) {
                 if (friendDataSnapshot.exists()) {
+                    // friendDataSnapshot에서 UserData 클래스로 데이터 변환
                     val friendData = friendDataSnapshot.getValue(UserData::class.java)
+                    // 읽어온 친구 데이터를 friendList에 추가
                     if (friendData != null) {
                         friendList.add(friendData)
+                        // 만약 마지막 친구 데이터를 가져왔다면, 어댑터에 변경을 알림.
                         if(isLast)
                             friendadapter.notifyDataSetChanged()
                     }
                 } else {
+                    // 해당 친구 데이터가 존재하지 않을 경우, 로그에 메시지 기록
                     Log.d("FirebaseDatabase", "Friend data not found for UID: $friendUid")
                 }
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
+                // 데이터베이스 조회가 실패한 경우 오류 로그 기록
                 Log.d("FirebaseDatabase", "onCancelled", databaseError.toException())
             }
         })
