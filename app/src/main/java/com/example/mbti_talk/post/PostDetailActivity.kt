759b8b9a236d8821c2c47fc7c392c0305455ac27
jang0710 <janga9710@naver.com>
2023-10-26 11:14:31 +0900

    [FEAT] : ⚙️ PostFragment.kt 리사이클러뷰에 아이템을 누르면 PostDetailActivity.kt로 이동 하게 된다. PostDetailActivity에서 상세 내용을 볼 수 있다.

--- app/src/main/java/com/example/mbti_talk/post/PostDetailActivity.kt
+++ app/src/main/java/com/example/mbti_talk/post/PostDetailActivity.kt
@@ -2,11 +2,35 @@ package com.example.mbti_talk.post
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.mbti_talk.R
+import com.bumptech.glide.Glide
+import com.example.mbti_talk.databinding.ActivityPostDetailBinding
+import com.google.firebase.storage.FirebaseStorage
 
 class PostDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_detail)
+        binding = ActivityPostDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra("title")
+        val content = intent.getStringExtra("content")
+        val image = intent.getStringExtra("image")
+
+        binding.PostDetailTitle.text = title
+        binding.PostDetailEtContent.text = content
+
+        val storage = FirebaseStorage.getInstance()
+        val reference = storage.reference.child("images/$image")
+        reference.downloadUrl.addOnSuccessListener {
+            Glide.with(this)
+                .load(it)
+                .into(binding.PostDetailImage)
+        }
+
+
+        binding.PostDetailBackarrow.setOnClickListener {
+            finish()
+        }
     }
 }
