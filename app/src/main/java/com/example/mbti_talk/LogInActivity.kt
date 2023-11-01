03be09b8789411cc772ce12bbba9e3c7322ae381
limduhyoung <limduh2@naver.com>
2023-11-01 21:40:46 +0900

    SignUpActivity2->googlelogin추가

--- app/src/main/java/com/example/mbti_talk/LogInActivity.kt
+++ app/src/main/java/com/example/mbti_talk/LogInActivity.kt
@@ -209,9 +209,7 @@ class LogInActivity : AppCompatActivity() {
             val credential = GoogleAuthProvider.getCredential(account.idToken, null)
             firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
                 if (task.isSuccessful) {
-                    //로그인후 넘어가는 곳 변경해봄 (바텀네비연결)
-//                    val intent = Intent(this, MainActivity::class.java)
-                    val intent = Intent(this, BottomActivity::class.java)
+                    val intent = Intent(this, SignUpActivity2::class.java)
                     startActivity(intent)
 
                     Toast.makeText(this, "계정이 생성되었습니다.", Toast.LENGTH_SHORT).show()
