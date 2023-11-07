907d388645e8cb62d344ea505ae3322dafd59a2f
Jblee <hicracks@gmail.com>
2023-11-07 20:41:56 +0900

    # filter

--- app/src/main/java/com/example/mbti_talk/Main/FilterDialogFragment.kt
+++ app/src/main/java/com/example/mbti_talk/Main/FilterDialogFragment.kt
@@ -12,7 +12,8 @@ import com.example.mbti_talk.R
 import com.example.mbti_talk.databinding.FragmentDialogFilterBinding
 
 class FilterDialogFragment : DialogFragment() {
-    var buttonClickListener: OnDialogChipClickListener? = null
+
+    private lateinit var buttonClickListener: OnDialogChipClickListener
 
     var gender = ""
     var minValue = 0
@@ -95,7 +96,8 @@ class FilterDialogFragment : DialogFragment() {
 
         // "적용" 버튼 클릭 이벤트 처리
         binding.applyBtn.setOnClickListener {
-            dismiss()
+            minValue = binding.filterSliderAge.values[0].toInt()
+            maxValue = binding.filterSliderAge.values[1].toInt()
 
             if (binding.chipGroupGender.checkedChipId == View.NO_ID ||
                 binding.chipGroupMbtiEI.checkedChipId == View.NO_ID ||
@@ -105,26 +107,25 @@ class FilterDialogFragment : DialogFragment() {
                 // 하나 이상의 Chip 그룹이 선택되지 않았을 때
                 Toast.makeText(requireContext(), "MBTI를 모두 선택해주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                buttonClickListener?.onChipGroubGender(gender)
-                buttonClickListener?.filterSliderAge(minValue, maxValue)
-                buttonClickListener?.onChipGroupMbtiEI(mbtiEI)
-                buttonClickListener?.onChipGroupMbtiSN(mbtiSN)
-                buttonClickListener?.onChipGroupMbtiTF(mbtiTF)
-                buttonClickListener?.onChipGroupMbtiJP(mbtiJP)
-                // 선택된 옵션에 대한 처리
-                Log.d("jblee", "slider min=$minValue, max = $maxValue")
+                buttonClickListener.onChipApply(gender,minValue, maxValue,mbtiEI,mbtiSN,mbtiTF,mbtiJP)
             }
+
+            Log.d("FilterDialogFragment","#jblee > $gender, $minValue, $maxValue,$mbtiEI,$mbtiSN,$mbtiTF,$mbtiJP")
+
+            dismiss()
+
         }
         return binding.root
     }
     interface OnDialogChipClickListener {
-        fun onChipGroubGender(gender: String)
-        fun filterSliderAge(minValue: Int, maxValue: Int)
-        fun onChipGroupMbtiEI(mbtiEI: String)
-        fun onChipGroupMbtiSN(mbtiSN: String)
-        fun onChipGroupMbtiTF(mbtiTF: String)
-        fun onChipGroupMbtiJP(mbtiJP: String)
+        fun onChipApply(gender: String, minValue: Int, maxValue: Int,mbtiEI: String, mbtiSN: String, mbtiTF: String, mbtiJP: String)
     }
+
+    fun setChipClickListener(chipClickListener: OnDialogChipClickListener) {
+        Log.d("FilterDialogFragment","#jblee > setChipClickListener")
+        this.buttonClickListener = chipClickListener
+    }
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return super.onCreateDialog(savedInstanceState).apply {
