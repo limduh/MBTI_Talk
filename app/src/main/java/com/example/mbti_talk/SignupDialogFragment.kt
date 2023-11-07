2c2ae939c3cab3b6d89a741d89d756592fea2ffb
limduhyoung <limduh2@naver.com>
2023-11-07 22:39:46 +0900

    dialog add

--- app/src/main/java/com/example/mbti_talk/SignupDialogFragment.kt
+++ app/src/main/java/com/example/mbti_talk/SignupDialogFragment.kt
@@ -35,7 +35,7 @@ class SignupDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.dialog_signup, container, false)
+        return inflater.inflate(R.layout.dialog_signout, container, false)
     }
 
     companion object {
